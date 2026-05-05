6fb6a192ae94da57818fa8e5a9bacb08a9d1149e
mostafa <talhaibnobayedallah@gmail.com>
2026-05-05 18:17:09 +0300

    making direct ellipse the >> class constructor take 3 points __pairs__ and return verctor of points

---
+++ ellipse.h
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <utility>
+#include <vector>
+
+class Ellispe {
+private:
+    std::vector<std::pair<int, int>> ellipse;
+    std::pair<int, int> center, a, b;
+    char m;
+
+    std::vector<std::pair<int, int>> direct(std::pair<int, int> c,
+                                            std::pair<int, int> a,
+                                            std::pair<int, int> b) {
+        double a2 = (double)a.first * a.first;
+        int x = 0;
+
+        while (x <= a.first) {
+            // Essential Fix: Use b.second and force double division
+            int y = std::round(b.second * std::sqrt(1.0 - ((double)x * x) / a2));
+
+            // Essential Fix: Add symmetry (4 points) and center offset
+            ellipse.push_back({ c.first + x, c.second + y });
+            ellipse.push_back({ c.first - x, c.second + y });
+            ellipse.push_back({ c.first + x, c.second - y });
+            ellipse.push_back({ c.first - x, c.second - y });
+            x++;
+        }
+        return ellipse;
+    }
+
+public:
+    Ellispe(std::pair<int, int> Center, std::pair<int, int> a_Horizontal, std::pair<int, int> b_vertical, char mode) {
+        center = Center;
+        a = a_Horizontal;
+        b = b_vertical;
+        m = mode;
+    }
+
+    std::vector<std::pair<int, int>> draw_ellipse(std::pair<int, int> c,
+                                                std::pair<int, int> a,
+                                                std::pair<int, int> b,
+                                                char m) {
+        ellipse.clear(); // Clear old points
+        a.second = c.second;
+        b.first = c.first;
+        switch (m) {
+            case 'd':
+                return direct(c, a, b);
+        }
+        return ellipse;
+    }
+};
